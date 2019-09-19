23350c2f6f31635eb7001458911b367205b1897b
amandachess <55164362+amandachess@users.noreply.github.com>
2019-09-18 22:15:47 -0300

    Atualização de BG
    Definição de plano de fundo

--- view/pages/Home.php
+++ view/pages/Home.php
@@ -1,25 +1,18 @@
 <?php include __DIR__ . "/../header.php";?>
-    
-    <input type="checkbox" id="click">
-    <label for="click" id="icone"><img src="img/icone.gif"></label>
-
-    <div class="barra">
-        <nav>
-            <a href="">
-                <div class="link"><img src="img/BPlayPassed.png"></div>
-            </a>
-            <a href="">
-                <div class="link"><img src="img/BLoginPassed.png"></div>
-            </a>
-            <a href="">
-                <div class="link"><img src="img/BCadastroPassed.png"></div>
-            </a>
-        </nav>
-    </div>
-    <div class="logoCirculo">
-        <img src="img/Circulologo.png">
-    </div>
-    <div class="logoPrincipal">
-        <img style="margin-left: 50px;" src="img/Logo.png">
+<style>
+.container{
+    margin: 0px;
+    padding: 0px;
+}
+</style>
+    <div>
+        <canvas id="canvas"></canvas>
+        <script  src="js/scriptBG.js"></script>
+        <div class="logoCirculo">
+            <img src="img/Circulologo.png">
+        </div>
+        <div class="logoPrincipal">
+            <img src="img/Logo.png">
+        </div>
     </div>
 <?php include __DIR__ . "/../footer.php";?>
