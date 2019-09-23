a35bccbf2fc342507acd0d1c121334d65cf1cabc
Everton Cardoso <everton_cardoso@live.com>
2019-09-22 23:44:01 -0300

    Organização do codigo dos enigmas

--- view/navbar.php
+++ view/navbar.php
@@ -10,4 +10,22 @@
             <li class="nav-item"><a class="nav-link" href="index.php?pagina=login">Ranking</a></li>
         </ul>
     </div>
+    <div class="profile">
+        <div class="photo"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/764024/profile/profile-512.jpg"/></div>
+            <div class="content">
+                <div class="text">
+                <h5>Usuário</h5>
+                </div>
+                <div class="btn btn-profile"><span></span></div>
+            </div>
+        <div class="box"><i class="fa fa-codepen"></i><i class="fa fa-facebook"></i><i class="fa fa-github"></i><i class="fa fa-tumblr"></i><i class="fa fa-twitter"></i></div>
+    </div>
 </nav>
+<script>
+    (function() {
+        $('.btn-profile').click(function() {
+            $(this).toggleClass('active');
+            return $('.box').toggleClass('open');
+        });
+    }).call(this);
+</script>
--- view/pages/Enigma.php
+++ view/pages/Enigma.php
@@ -5,8 +5,13 @@
         <input type="text" class="404-input">
     </form>
     <div class="terminal">
-        <p class="prompt">Eu nunca fui, mas sempre serei. Ninguém nunca me viu, e nunca verão. Ainda assim, sou a esperança de todos. Quem sou eu?</p>
+        <p class="prompt question"></p>
         <p class="prompt output new-output"></p>
     </div>
 </div>
-<script  src="js/enigma-engine.js"></script>
+<div class="picker picker-background"></div>
+<div class="picker picker-color"></div>
+
+<script  src="js/enigma/Engine.js"></script>
+<script  src="js/enigma/Controller.js"></script>
+<script  src="js/enigma/index.js"></script>
