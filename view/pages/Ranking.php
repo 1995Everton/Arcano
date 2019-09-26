8968cee2b219f77da566d44425933a7b20704456
Everton Cardoso <everton_cardoso@live.com>
2019-09-26 00:02:34 -0300

    Table Sortable

--- view/pages/Ranking.php
+++ view/pages/Ranking.php
@@ -1,22 +1,51 @@
-<table class="table">
+<!--<table class="table">-->
+<!--    <thead>-->
+<!--        <tr>-->
+<!--            <th scope="col">#</th>-->
+<!--            <th scope="col">Nome</th>-->
+<!--            <th scope="col">Pontuação</th>-->
+<!--            <th scope="col">Progresso</th>-->
+<!--            <th scope="col">Data</th>-->
+<!--        </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--        --><?php //foreach ($ranking as $key =>$usuario):?>
+<!--            <tr>-->
+<!--                <th scope="row">--><?//= $key;?><!--</th>-->
+<!--                <td>--><?//= $usuario['nome_usuario'];?><!--</td>-->
+<!--                <td>--><?//= $usuario['pontos'];?><!--</td>-->
+<!--                <td>--><?//= $usuario['progresso']."%";?><!--</td>-->
+<!--                <td>--><?//= date("d/m/Y", strtotime($usuario['data']));?><!--</td>-->
+<!--            </tr>-->
+<!--        --><?php //endforeach;?>
+<!--    </tbody>-->
+<!--</table>-->
+<link rel="stylesheet" href="css/table.css">
+<table>
     <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Pontuação</th>
-            <th scope="col">Progresso</th>
-            <th scope="col">Data</th>
-        </tr>
+    <tr>
+        <th>Nº</th>
+        <th>Nome</th>
+        <th>Pontuação</th>
+        <th>Progresso</th>
+        <th>Data</th>
+    </tr>
     </thead>
     <tbody>
         <?php foreach ($ranking as $key =>$usuario):?>
             <tr>
-                <th scope="row"><?= $key;?></th>
-                <td><?= $usuario['nome_usuario'];?></td>
-                <td><?= $usuario['pontos'];?></td>
-                <td><?= $usuario['progresso']."%";?></td>
-                <td><?= date("d/m/Y", strtotime($usuario['data']));?></td>
+                <td data-label="Nº"><?= $key;?></td>
+                <td data-label="Nome"><?= $usuario['nome_usuario'];?></td>
+                <td data-label="Pontuação"><?= $usuario['pontos'];?></td>
+                <td data-label="Progresso"><?= $usuario['progresso']."%";?></td>
+                <td data-label="Data"><?= date("d/m/Y", strtotime($usuario['data']));?></td>
             </tr>
         <?php endforeach;?>
     </tbody>
 </table>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.30.1/js/jquery.tablesorter.min.js'></script>
+<script>
+    $(function(){
+        $('table').tablesorter();
+    });
+</script>
