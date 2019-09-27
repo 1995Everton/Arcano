5575f2effa9bde61240d150bdc3eee7c8d4d769d
amandachess <55164362+amandachess@users.noreply.github.com>
2019-09-26 21:38:17 -0300

    Excluído imagens não utilizadas

--- view/pages/Home.php
+++ view/pages/Home.php
@@ -1,15 +1,9 @@
 <link rel="stylesheet" type="text/css" href="css/home.css">
-<style>
-.container{
-    margin: 0px;
-    padding: 0px;
-}
-</style>
 
-    <img style="position: fixed;width: 100%; height: 100%" src="img/star.gif">
-    <div class="logoCirculo">
-        <img src="img/Circulologo.png">
-    </div>
-    <div class="logoPrincipal">
-        <img src="img/Logo.png">
-    </div>
+<img style="position: fixed;width: 100%; height: 100%" src="img/star.gif">
+<div class="logoCirculo">
+    <img src="img/Circulologo.png">
+</div>
+<div class="logoPrincipal">
+    <img src="img/Logo.png">
+</div>
