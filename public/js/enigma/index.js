4aaf74e1008543e0cd0ebcf63e4defbf7d8159f2
Everton Cardoso <everton_cardoso@live.com>
2019-09-27 21:47:35 -0300

    Removido novo login

--- public/js/enigma/index.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    color : '#1ff042',
-    background: '#060e1b',
-    pergunta: 'Eu nunca fui, mas sempre serei. Ninguém nunca me viu, e nunca verão. Ainda assim, sou a esperança de todos. Quem sou eu?',
-    resposta : 'amanha',
-    dica:  'Não é Hoje'
-}
-new Controller($('.404-input'),config);
- 
