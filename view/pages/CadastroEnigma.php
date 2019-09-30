38d5089a041716304f2b6bfa9dfbc5debc201e58
Alisson Pospor <ali_pospor@hotmail.com>
2019-09-30 20:56:38 -0300

    Atualização tela Cadastro Enigma
    Foi adicionado Pesquisa no banco de dados para associar o Nivel de ajuda da DICA.

--- view/pages/CadastroEnigma.php
+++ view/pages/CadastroEnigma.php
@@ -1,4 +1,25 @@
-<form action="index.php?pagina=persistencia-enigma">
-
-<button type="submit">Joia</button>
+<?php
+    /* Pesquisa de categoria DICA no banco de dados */
+    $ds_categoria = $this->banco->select("SELECT * FROM categorias");
+?>
+<form action="index.php?pagina=enigma-persistencia" method="POST">
+	<label for="default_select" class="text-white">Default select</label>
+	<div class="nes-select">
+		<select name="default_select">
+			<?php
+            foreach($ds_categoria as $key =>$valor){
+            ?>
+			<option value="<?= $ds_categoria[$key]['id_categoria_dica'];?>">
+				<?= $ds_categoria[$key]['ds_categoria']; ?>
+			</option>
+			<?php
+            }
+            ?>
+		</select>
+	</div>
+	<div class="nes-field">
+		<label for="name_field" class="text-white">Your name</label>
+		<input type="text" id="name_field" class="nes-input">
+	</div>
+	<button type="submit" class="nes-btn is-success">Success</button>
 </form>
