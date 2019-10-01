c9bd9daa0bd332bc8ad9e46a3896d1e8e156f0f8
LeandroSS04 <rjleandrosoares@gmail.com>
2019-09-30 22:15:34 -0300

    deu ruim

--- src/Controllers/PersistenciaUsuario.php
+++ src/Controllers/PersistenciaUsuario.php
@@ -8,60 +8,65 @@ class PersistenciaUsuario extends Banco implements RequestHandlerInterface
 {
     public function handle()
     {
-             
+        
             $usuario = $_POST['usuario'];
             $email = $_POST['email'];
             $senha = $_POST['senha'];
             $confsenha = $_POST['confsenha'];
 
             $dados = $this->banco->select("SELECT * FROM usuarios WHERE nome_usuario = ? OR email = ?",[$usuario, $email],true);
+   
+            if ($dados) {
+                foreach ($dados as $key => $value) {
+                    if ($value['nome_usuario'] == $usuario ) {
+                        
+                        header("Location: index.php?pagina=cadastro&msg=2");
+                        
+                    }
+                    elseif ($value['email'] == $email) {
+                        $this->toast('Email já cadastrado','ERRO','danger ');
+                        header("Location: index.php?pagina=cadastro&msg=3");
+                    }
+                }
     
-            
-            
-            foreach ($dados as $key => $value) {
-                if ($value->nome_usuario == $usuario ) {
-                    
-                    header("Location: index.php?pagina=cadastro&msg=2");
+                if(filter_var($email, FILTER_VALIDATE_EMAIL))
+                {
                     
                 }
-                elseif ($value->email == $email) {
-                    header("Location: index.php?pagina=cadastro&msg=3");
+                else
+                {
+                    header("Location: index.php?pagina=cadastro&msg=1");
                 }
-            }
-
-            if(filter_var($email, FILTER_VALIDATE_EMAIL))
-            {
+    
                 
-            }
-            else
-            {
-                header("Location: index.php?pagina=cadastro&msg=1");
-            }
+    
+                if ($senha != $confsenha) {
+    
+    
+                    header("Location: index.php?pagina=cadastro&msg=4");
+                   
+                }  
 
-            
-
-            if ($senha != $confsenha) {
-
-
-                header("Location: index.php?pagina=cadastro&msg=4");
-               
-            }
-            
-            $this->banco->insert('usuarios',
+            }else {
+                $this->banco->insert('usuarios',
                 [
                     'categoria_usuarios_id' => 2,
                     'nome_usuario' => $usuario,
                     'senha' => $this->criptografar($senha),
                     'email' => $email
                 ]
-            );
-            header("Location: index.php?pagina=login");
+                );
+                header("Location: index.php?pagina=login");
+            }
+            
+            
+            
 
             //criar função para validar senha, na barra de favoritos tem sites para ajudar
             //$retorno = $this->usuarios->cadastrarUsuario($nome_usuario,2,$email,$senha)
         
             //echo $usuario, $email, $senha, $confsenha;
-            
+        
         
     }
 }
--- view/pages/cadastro.php
+++ view/pages/cadastro.php
@@ -1,27 +1,29 @@
+<div>
 <form action="index.php?pagina=persistenciausuario" method="POST">
   <div class="form-row">
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="usuario" placeholder="Usuario" required>
+        <input type="text" class="nes-input is-dark" name="usuario" placeholder="Usuario" required>
     </div>
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="email" placeholder="Email" required>
+        <input type="text" class="nes-input is-dark" name="email" placeholder="Email" required>
     </div>
     <div class="w-100"></div>
     <div class="form-group col-md-6">
         
-        <input type="password" class="form-control" id="senha" name="senha" pattern="^(?=.*[a-zA-Z])(?=\w*[0-9])\w{6,12}$" placeholder="Senha"  > <!---->
+        <input type="password" class="nes-input is-dark" id="senha" name="senha" pattern="^(?=.*[a-zA-Z])(?=\w*[0-9])\w{6,12}$" placeholder="Senha"  > <!---->
     </div>
     <div class="form-group col-md-6">
-        <input type="password" class="form-control" name="confsenha"  placeholder="Confirmar Senha" required> 
+        <input type="password" class="nes-input is-dark" name="confsenha"  placeholder="Confirmar Senha" required> 
     </div>
     <div class="w-100"></div>
-    <div class="form-group col-md-6">
+    <!--<div class="form-group col-md-6">-->
     
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
+        <button type="submit" class="nes-btn is-primary">Cadastrar</button>
         
-    </div>
+    <!--</div>-->
   </div>
 </form>
+</div>
 <script>
 
 var senha = document.getElementById("senha");
