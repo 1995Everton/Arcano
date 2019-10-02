22608daf3acce737cd49c5654aa1a6dc9b9f78e4
Alisson Pospor <ali_pospor@hotmail.com>
2019-10-01 21:23:08 -0300

    Campos para cadastro de Enigma
    Adicionado Todos os campos para cadastro de enigma no formulario.

--- src/Controllers/PersistenciaEnigma.php
+++ src/Controllers/PersistenciaEnigma.php
@@ -8,6 +8,22 @@ class PersistenciaEnigma extends Banco implements RequestHandlerInterface
 {
     public function handle()
     {
-        var_dump( $_POST);
+        if(isset($_POST)){
+            echo "<pre>";
+            var_dump($_POST);
+            echo "</pre>";
+
+            echo "<br>";
+            
+            /* data */
+            $data = date('Y-m-d');
+
+            /* ARQUIVO */
+            echo "<pre>";
+            print_r($_FILES['arquivo']);
+            echo "</pre>";
+            /* https://www.php.net/manual/pt_BR/function.move-uploaded-file.php */
+            /* https://www.php.net/manual/pt_BR/features.file-upload.post-method.php */
+        }
     }
 }
--- view/pages/CadastroEnigma.php
+++ view/pages/CadastroEnigma.php
@@ -3,9 +3,10 @@
 	$ds_categoria = $this->banco->select("SELECT * FROM categorias");
 	/* Pesquisa do tipo do enigma para cadastro */
 	$ds_tipos = $this->banco->select("SELECT * FROM tipos");
-	var_dump($ds_tipos);
+	$data = date('Y-m-d');
+	echo "<h2 class='text-white'>". $data ."</h2>";
 ?>
-<form action="index.php?pagina=enigma-persistencia" method="POST" class="sky text-white">
+<form action="index.php?pagina=enigma-persistencia" enctype="multipart/form-data" method="POST" class="sky text-white">
 	<!-- Campo Dificuldade enigma -->
 	<label for="dificuldade">Dificuldade:</label>
 	<div class="nes-select">
@@ -22,20 +23,34 @@
 		</select>
 	</div>
 	<!-- Campo -> Tipo do enigma -->
+	<label for="tipo">Tipo:</label>
 	<div class="nes-select">
 		<select name="tipo" id="tipo">
 			<?php
 			foreach ($ds_tipos as $key => $valor) {
 			?>
-			<option value="<?= $ds_tipos[$key]?>"></option>
+			<option value="<?= $ds_tipos[$key]['id_tipos']?>">
+				<?= $ds_tipos[$key]['ds_tipo'] ?>
+			</option>
+			<?php
+			}
+			?>
 		</select>
 	</div>
 	<!-- Campo -> Enigma -->
 	<div class="nes-field">
-		<label for="">Enigma:</label>
+		<label for="enigma">Enigma:</label>
 		<input type="text" name="enigma" id="enigma" class="nes-input">
 	</div>
-
-
+	<!-- Arquivo -->
+	<div class="nes-field">
+		<label for="arquivo" class="nes-field">Arquivo:</label>
+		<input type="file" name="arquivo" id="arquivo" class="nes-input">
+	</div>
+	<!-- Resposta -->
+	<div class="nes-field">
+		<label for="resposta">Resposta:</label>
+		<input type="text" name="resposta" id="resposta" class="nes-input">
+	</div>
 	<button type="submit" class="nes-btn is-success">Success</button>
 </form>
