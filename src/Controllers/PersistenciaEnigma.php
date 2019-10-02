9bf62c7755dee65936727599fe7d5bf24fdff224
Alisson Pospor <ali_pospor@hotmail.com>
2019-10-01 22:14:48 -0300

    Add move_uploaded_file
    Adicionado fórmula genérica para se usar move_uploaded_file, no FORM de cadastro enigma.

--- src/Controllers/PersistenciaEnigma.php
+++ src/Controllers/PersistenciaEnigma.php
@@ -22,8 +22,19 @@ class PersistenciaEnigma extends Banco implements RequestHandlerInterface
             echo "<pre>";
             print_r($_FILES['arquivo']);
             echo "</pre>";
+
+            /* Mover o file // diretório*/
+            /* Cheio de coisa para fazer aqui!! */
+            $diretorio = '../public/img/';
+            $arquivoUPADO = $diretorio . basename($_FILES['arquivo']['name']);
+            
+            if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $arquivoUPADO)) {
+                echo "Arquivo válido e enviado com sucesso.\n";
+            }
+
             /* https://www.php.net/manual/pt_BR/function.move-uploaded-file.php */
             /* https://www.php.net/manual/pt_BR/features.file-upload.post-method.php */
+            /* https://www.youtube.com/watch?v=qmNbZmBOaGM */
         }
     }
 }
