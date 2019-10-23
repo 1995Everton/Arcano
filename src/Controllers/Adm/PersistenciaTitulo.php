8fd7da50b2951d211c8c4dee74477389787f2ed3
LeandroSS04 <rjleandrosoares@gmail.com>
2019-10-22 21:36:00 -0300

    Crud Título

---
+++ src/Controllers/Adm/PersistenciaTitulo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Arcanos\Enigmas\Controllers\Adm;
+
+
+use Arcanos\Enigmas\Controllers\Banco;
+use Arcanos\Enigmas\Controllers\RequestHandlerInterface;
+
+class PersistenciaTitulo extends Banco implements RequestHandlerInterface
+{
+    public function handle()
+    {
+        $titulo = $_POST['titulo'];
+
+        $dados = $this->banco->select("SELECT * FROM titulo WHERE ds_titulo = ? ", [$titulo], true);
+
+        if ($dados) {
+            foreach ($dados as $key => $value) {
+                if ($value['ds_titulo'] == $titulo) {
+                    $this->toast('Título já cadastrado', 'ERRO', 'danger');
+                    header("Location: index.php?pagina=cadastro-titulo");
+
+                }
+            }
+        } else {
+            $this->banco->insert('titulo',
+                [
+                    'ds_titulo' => $titulo,
+                ]
+            );
+            $this->toast('Cadastro de título realizado com susseso', '', 'success');
+            header("Location: index.php?pagina=cadastro-titulo");
+        }
+    }
+}
