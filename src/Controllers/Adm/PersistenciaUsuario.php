34ad962de0d691dc47b731ddf7a41d1e878726c5
Everton Cardoso <everton_cardoso@live.com>
2019-10-23 09:46:32 -0300

    Modal

---
+++ src/Controllers/Adm/PersistenciaUsuario.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Arcanos\Enigmas\Controllers\Adm;
+
+
+use Arcanos\Enigmas\Controllers\Banco;
+use Arcanos\Enigmas\Controllers\RequestHandlerInterface;
+
+class PersistenciaUsuario extends Banco implements RequestHandlerInterface
+{
+
+    public function handle()
+    {
+
+    }
+}
