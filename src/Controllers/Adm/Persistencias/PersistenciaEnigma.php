0af7a91ccf50e4695e6d5b3f06cd671a7e54aaa4
Alisson Pospor <ali_pospor@hotmail.com>
2019-11-28 22:22:06 -0300

    CRUD ainda não terminado, não esta salvando no banco

--- src/Controllers/Adm/Persistencias/PersistenciaEnigma.php
+++ src/Controllers/Adm/Persistencias/PersistenciaEnigma.php
@@ -22,64 +22,22 @@ class PersistenciaEnigma extends Banco implements RequestHandlerInterface
                 $this->deletar();
                 break;
             default:
-                header("Location: index.php?pagina=form-enigma");
+                header("Location: index.php?pagina=tabela-enigma");
                 break;
-        }
-    }
-
-    public function validarPost()
-    {
-        $erro = null;
-        $URL = NULL;
-
-        var_dump($_POST);
-        /* Validacao dos campos do formulário */
-        if (!isset($_POST['dificuldade']) || (empty($_POST['dificuldade']))) {
-            $erro = 'Verifique o campo de Dificuldade';;
-        } elseif ((!isset($_POST['tipo'])) || ($_POST['tipo'])) {
-            $erro = 'Verifique o campo Tipo';
-        } elseif ((!isset($_POST['resposta'])) || (empty($resposta))) {
-            $erro = 'Verifique o campo Resposta';
-        } elseif ($_POST['tipo'] == 1) {
-            if (!isset($_POST['enigma']) || (empty($_POST['enigma']))) {
-                $erro = 'Preencha corretamente o campo Enigma!';
-            }
-        }
-
-        /* cadastro de arquivo e validação do arquivo */
-        if ($_POST['tipo'] != 1) {
-            $arquivo_temporario = $_FILES['arquivo']['tmp_name'];
-            $nome_arquivo = date_timestamp_get(date_create()) . basename($_FILES['arquivo']['name']);
-            $diretorio = '../public/img/uploaded_arqs/';
-
-            if (move_uploaded_file($arquivo_temporario, $diretorio . $nome_arquivo)) {
-                $URL = "http://" . $_SERVER['SERVER_NAME'] . "/" . "Arcano" . "/public/img/uploaded_arqs/" . $nome_arquivo; //url que vai para o banco
-            } else {
-                $erro = 'Erro no envio do arquivo!';
-            }
-        } else {
-            $erro = 'Se enigma for do tipo: Imagem, Audio ou Video é necessario anexar uma arquivo!';
-        }
-
-
-        if (!isset($erro)) {
-            echo 'final bom';
-        } else {
-            echo 'final ruim';
-            $this->toast($erro, 'ERRO', 'danger');
-            //header("Location: index.php?pagina=form-enigma");
         }
     }
 
     public function novoEnigma()
     {
+        $enigma = null;
+
         if ($this->validarPost() == true) {
             $this->banco->insert(
                 'enigmas',
                 [
                     'dificuldade_enigma_id' => $_POST['dificuldade'],
                     'enigmas_tipos_id' => $_POST['tipo'],
-                    'enigma' => (empty($_POST['enigma'])) ? $_POST['enigma'] : arquivo(),
+                    'enigma' => $enigma = ($_POST['tipo'] == 1) ? $_POST['enigma'] : $this->arquivoUpload($_FILES['arquivo']['tmp_name']),
                     'data' => date('Y-m-d'),
                     'resposta' => $_POST['resposta']
                 ]
@@ -89,13 +47,14 @@ class PersistenciaEnigma extends Banco implements RequestHandlerInterface
 
     public function editar()
     {
+        $enigma = null;
         if ($this->validarPost() == true) {
             $this->banco->update(
                 'enigmas',
                 [
                     'dificuldade_enigma_id' => $_POST['dificuldade'],
                     'enigmas_tipos_id' => $_POST['tipo'],
-                    'enigma' =>  $_POST['enigma'],
+                    'enigma' =>  $enigma = ($_POST['tipo'] == 1) ? $_POST['enigma'] : $this->arquivoUpload($_FILES['arquivo']['tmp_name']),
                     'data' => date('Y-m-d'),
                     'resposta' => $_POST['resposta']
                 ],
@@ -114,5 +73,64 @@ class PersistenciaEnigma extends Banco implements RequestHandlerInterface
                 'id_enigmas' => $_GET['id']
             ]
         );
+    }
+
+
+    public function validarPost()
+    {
+        $erro = null;
+
+        var_dump($_POST);
+        /* Validacao dos campos do formulário */
+        if (!isset($_POST['dificuldade']) || (empty($_POST['dificuldade']))) {
+            $erro = 'Verifique o campo de Dificuldade';
+            echo 'Verifique o campo de Dificuldade';
+        } elseif ((!isset($_POST['tipo'])) || (empty($_POST['tipo']))) {
+            $erro = 'Verifique o campo Tipo';
+            echo 'Verifique o campo Tipo';
+        } elseif ((!isset($_POST['resposta'])) || (empty($_POST['resposta']))) {
+            $erro = 'Verifique o campo Resposta';
+            echo  'Verifique o campo Resposta';
+        } elseif ($_POST['tipo'] == 1) {
+            if (!isset($_POST['enigma']) || (empty($_POST['enigma']))) {
+                $erro = 'Preencha corretamente o campo Enigma!';
+                echo 'Preencha corretamente o campo Enigma!';
+            }
+        } elseif (($_POST['tipo'] != 1) && (!isset($_FILES['arquivo']['name']))) {
+            $erro = 'Se enigma for do tipo: Imagem, Audio ou Video é necessario anexar uma arquivo!';
+            echo 'Se enigma for do tipo: Imagem, Audio ou Video é necessario anexar uma arquivo!';
+        }
+
+        if (!isset($erro)) {
+            echo 'final bom';
+        } else {
+            echo 'final ruim';
+            $this->toast($erro, 'ERRO', 'danger');
+            //header("Location: index.php?pagina=form-enigma");
+        }
+    }
+
+
+    public function arquivoUpload()
+    {
+        $URL = false;
+        $nome_arquivo = null;
+        $diretorio = '../public/img/';
+
+        /* cadastro de arquivo e validação do arquivo */
+        if (($_POST['tipo'] != 1) && (isset($_FILES['arquivo']['tmp_name']))) {
+            $nome_arquivo = date_timestamp_get(date_create()) . basename($_FILES['arquivo']['name']);
+
+            if (move_uploaded_file($_FILES['arquivo']['tmp_name'],  $diretorio . $nome_arquivo)) {
+                $URL = "http://" . $_SERVER['SERVER_NAME'] . "/" . "Arcano" . $diretorio . $nome_arquivo; //url que vai para o banco
+                return $URL;
+            } else {
+                $this->toast('Falha no upload do arquivo', 'ERRO', 'danger');
+                //header("Location: index.php?pagina=form-enigma");
+            }
+        } else {
+            $this->toast('Falha no upload do arquivo', 'ERRO', 'danger');
+            //header("Location: index.php?pagina=form-enigma");
+        }
     }
 }
